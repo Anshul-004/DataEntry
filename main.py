2cadf7726aa32b3295f6dcc01e889c544af21d5a
Anshul Singh <anshulsingh024@yahoo.com>
2023-04-01 12:52:44 +0530

    added time functionality

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # make a program which saves the data you enter init, to a different text file , with time stamp
 
-import time
+from datetime import datetime
 import os
 
 print("Enter SPO2 level : ")
 a=input()
 
+now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) #returns dateandtime
+
+
 f = open("logfile.txt", "a")
-f.write(a+"\n")
+f.write(now+ "\t\t" +a+"\n")
