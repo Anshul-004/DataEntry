643654b9ae0c8636c7fafcf103cf5a0e4de4c3b7
Anshul Singh <anshulsingh024@yahoo.com>
2023-04-01 12:44:27 +0530

    added file system

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+# make a program which saves the data you enter init, to a different text file , with time stamp
+
+import time
+import os
+
+print("Enter SPO2 level : ")
+a=input()
+
+f = open("logfile.txt", "a")
+f.write(a+"\n")
