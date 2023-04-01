f4b2a2fd784bf279bf6581934fc0afaedc3079ea
Anshul Singh <anshulsingh024@yahoo.com>
2023-04-01 12:56:07 +0530

    made final  changes

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import os
 print("Enter SPO2 level : ")
 a=input()
 
+print("Enter Pulse level : ")
+b=input()
+
 now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) #returns dateandtime
 
 
 f = open("logfile.txt", "a")
-f.write(now+ "\t\t" +a+"\n")
+f.write(now+ "\t\t" +"SPO2 :"+a+"\t"+"Pulse :"+b+"\n")
