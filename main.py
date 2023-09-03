d01f48fec75abdb4215318cf0ad65a2dd312b68a
Anshul Singh <anshulsingh024@yahoo.com>
2023-09-03 13:51:03 +0530

    Edited metadata

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 # make a program which saves the data you enter init, to a different text file , with time stamp
 
 from datetime import datetime
-import os
 
-print("Enter Your Name : ")
-name=input()
+while(1):
+    print("1.Enter Details\n2.Check Logs\n3.Exit")
+    ch= int(input())
+    if ch==1:
+        print("Enter Your Name : ")
+        name=input()
 
-print("Enter Your Weight : ")
-weight=input()
+        print("Enter Your Weight : ")
+        weight=input()
 
-now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) #returns dateandtime
+        now = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) #returns dateandtime
 
 
-f = open("weightlogs.txt", "a")
-f.write(now+ "\t\t" +"Name : "+name+"\t"+"Weight : "+weight+"\n")
+        with open("weightlogs.txt", "+a") as f:
+            f.write(now+ "\t\t" +"Name : "+name+"\t"+"Weight : "+weight+"\n")
+    
+    elif ch==2:
+        with open("weightlogs.txt", "r") as f:
+            content=f.read()
+
+        print(content)
+    
+    elif ch==3:
+        break
+
+    else :
+        print("Invalid Input")
